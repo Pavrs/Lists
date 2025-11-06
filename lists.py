394f23b50bac56386d8f365e968e00c78b71e066
Pavel Stoimenov <ps05267498@priestley.ac.uk>
2025-11-06 10:44:36 +0000

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,48 @@
+# [program name]
+# Pavel Stoimenov
+# [date]
+# AS Computer Science
+
+# [comment]
+def one():
+    try:
+        #Empty list
+        nlist = []
+        pass   
+    except Exception as e:
+        print("Error occurred:", e )
+
+# [comment]
+def two():
+    try:
+        #list with n.o
+        scores = [12,14,52,81,90]
+        pass   
+    except Exception as e:
+        print("Error occurred:", e )
+
+def three():
+    try:
+        #quote list
+        colour = ['white','purple','azure','green','blue','red']
+        pass   
+    except Exception as e:
+        print("Error occurred:", e )
+
+def four():
+    try:
+        #mixed data types
+        record = [1, 'pav', 'student', 12, True]
+        pass   
+    except Exception as e:
+        print("Error occurred:", e )
+
+# [comment]
+def main():
+    try:
+        pass   
+    except Exception as e:
+        print("Error occurred:", e )    
+
+if (__name__ == "__main__"):
+    main()
